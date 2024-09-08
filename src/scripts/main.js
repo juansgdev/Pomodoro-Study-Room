77d179fb53413ca1c350610a6e4ad066995c1a0e
juansgdev <juangomes.profissional@gmail.com>
2024-09-07 21:42:31 -0300

    inserção da tela de login ou registro

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-alert('no ar');
+const loginButton = document.getElementById("login");
+const registerButton = document.getElementById("register");
+const formContainer = document.getElementById("form-container");
+
+const alternateForm = (button1, button2) => {
+    const pagePaths = [
+        "src/pages/login/login.html",
+        "src/pages/register/register.html"
+    ];
+
+    button1.disabled = true;
+    button2.disabled = false;
+
+    formContainer.innerHTML = "";
+    const iframe = document.createElement("iframe");
+    if (button1 == loginButton) {
+        iframe.src = pagePaths[0];
+    } else {
+        iframe.src = pagePaths[1];
+    }
+    formContainer.appendChild(iframe);
+};
+
+loginButton.onclick = () => {
+    alternateForm(loginButton, registerButton);
+};
+
+registerButton.onclick = () => {
+    alternateForm(registerButton, loginButton);
+};
