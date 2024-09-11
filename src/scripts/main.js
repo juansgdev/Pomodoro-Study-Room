8c0e7a2a44a809659e5a697c8b76425df6ae31f0
juansgdev <juangomes.profissional@gmail.com>
2024-09-11 19:29:43 -0300

    redirecionamento para dashboard feito

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,6 +13,7 @@ const alternateForm = (button1, button2) => {
 
     formContainer.innerHTML = "";
     const iframe = document.createElement("iframe");
+    iframe.setAttribute("id", "login-register");
     if (button1 == loginButton) {
         iframe.src = pagePaths[0];
     } else {
@@ -21,8 +22,25 @@ const alternateForm = (button1, button2) => {
     formContainer.appendChild(iframe);
 };
 
+const redirect = () => {
+    const iframe = document.getElementById("login-register");
+    iframe.onload = () => {
+        const iframeDoc = iframe.contentDocument;
+        iframeDoc.getElementById("submit-button").onclick = () => {
+            const inputs = iframeDoc.getElementsByTagName("input");
+            
+            if (inputs[0].value === "juan" && inputs[1].value === "123") {
+                window.location.replace("http://127.0.0.1:5500/src/pages/dashboard/dashboard.html")
+            }
+        };
+    };
+};
+
+redirect();
+
 loginButton.onclick = () => {
     alternateForm(loginButton, registerButton);
+    redirect();
 };
 
 registerButton.onclick = () => {
